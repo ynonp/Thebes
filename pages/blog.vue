0647d879790b30a1d575a2ca685553fe4620b6a2
ynonp <ynonperek@gmail.com>
2025-02-05 09:49:16 +0200

    initial commmit

---
+++ pages/blog.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>My Blog</h1>
+  <p>it's super interesting</p>
+  <a href="/">Back home</a>
+</template>
+
+<style lang="css" scoped>
+h1 {
+  border-bottom: 1px solid purple;
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const count = ref(0);
+</script>
+
+<template>
+  <h1>Welcome To Nuxt</h1>
+  <button @click="count++">{{ count }}</button>
+  <a href="/blog">Read My blog</a>
+</template>
